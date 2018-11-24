f09e735755621867399395a532fdb03c53d93812
Rex Lo <aesoph842732010@yahoo.com.hk>
2018-11-24 21:33:26 +0800

    Story 3 done

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -2,46 +2,62 @@ package com.oocl.cultivation;
 
 public class ParkingBoy {
 
-    private final ParkingLot parkingLot;
+    private ParkingLot[] parkingLots ;
+    private ParkingLot parkingLot;
     private String lastErrorMessage;
+    private int parkingLotNumber;
 
     public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        parkingLots = new ParkingLot[1];
+        parkingLots[0] = parkingLot;
+        parkingLotNumber = 1;
+        parkingLotNumber = parkingLots.length;
+    }
+
+    public ParkingBoy(ParkingLot[] parkingLots) {
+        parkingLotNumber = parkingLots.length;
+        parkingLots = new ParkingLot[parkingLotNumber];
+        this.parkingLots = parkingLots;
     }
 
     public ParkingTicket park(Car car) {
         // TODO: Please implement the method
-        if (parkingLot.getAvailableParkingPosition()>=1){
-            ParkingTicket parkingTicket = new ParkingTicket();
-            parkingLot.getCars().put(parkingTicket, car);
-            lastErrorMessage = null;
-            return parkingTicket;
-        }
-        else
-            {
+        for (int index = 0; index<parkingLotNumber; index++) {
+            if (parkingLots[index].getAvailableParkingPosition() >= 1) {
+                ParkingTicket parkingTicket = new ParkingTicket();
+                parkingLots[index].getCars().put(parkingTicket, car);
+                lastErrorMessage = null;
+                return parkingTicket;
+            } else {
+                if (index >= parkingLotNumber-1)
                 lastErrorMessage = "The parking lot is full.";
                 return null;
             }
-
+        }
+        return null;
     }
 
     public Car fetch(ParkingTicket ticket) {
         if (ticket != null) {
-            if (parkingLot.getCars().containsKey(ticket)) {
-                Car tempCar = parkingLot.getCars().get(ticket);
-                parkingLot.getCars().remove(ticket);
-                ticket.setUsed(true);
-                lastErrorMessage = null;
-                return tempCar;
-            } else{
-                lastErrorMessage = "Unrecognized parking ticket.";
-                return null;
+            for (int index=0; index<parkingLotNumber; index++) {
+                if (parkingLots[index].getCars().containsKey(ticket)) {
+                    Car tempCar = parkingLots[index].getCars().get(ticket);
+                    parkingLots[index].getCars().remove(ticket);
+                    ticket.setUsed(true);
+                    lastErrorMessage = null;
+                    return tempCar;
+                } else {
+                    if (index >= parkingLotNumber-1)
+                        lastErrorMessage = "Unrecognized parking ticket.";
+                        return null;
+                }
             }
         }
         else{
             lastErrorMessage = "Please provide your parking ticket.";
             return null;
         }
+        return null;
         //throw new RuntimeException("Not implemented");
     }
 
